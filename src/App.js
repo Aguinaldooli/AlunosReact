bb85424a132d2e3518bba0fb8bfc78977e2d2326
Aguinaldo <guiga_oli@hotmail.com>
2024-06-25 17:17:13 -0300

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import cadastro from './assets/cadastro.png'
+import { Modal, ModalBody, ModalFooter, ModalHeader, } from "reactstrap";
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const baseUrl = "https://localhost:44374/api/alunos"
+
+  const [data, setData] = useState([]);
+
+  const pedidosGet = async () => {
+    await axios.get(baseUrl)
+      .then(Response => {
+        setData(Response.data);
+      }).catch(error => {
+        console.log(error)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <br />
+      <h3>Cadastro de Alunos</h3>
+      <header>
+        <img src={cadastro} alt='Cadastro' />
+        <button className='btn btn-success'>Incluir novo Aluno</button>
       </header>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
     </div>
   );
 }
