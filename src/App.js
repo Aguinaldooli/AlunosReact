42df6eac4591f750be335808b20af3f3c01acef5
Aguinaldo <guiga_oli@hotmail.com>
2024-06-27 17:12:03 -0300

    feat: added other functions to the buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
 import './App.css';
 import axios from 'axios';
-import cadastro from './assets/cadastro.png'
-import { Button, Modal, ModalBody, ModalFooter, ModalHeader, } from "reactstrap";
+import cadastro from './assets/cadastro.png';
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 import { useState, useEffect } from 'react';
-
 
 function App() {
 
-  const baseUrl = "https://localhost:44374/api/alunos"
+  const baseUrl = "https://localhost:44374/api/alunos";
 
   const [data, setData] = useState([]);
+  const [modalIncluir, setModalIncluir] = useState(false);
+  const [modalEditar, setModalEditar] = useState(false);
 
-  const pedidosGet = async () => {
+  const [alunoSelecionado, setAlunoSelecionado] = useState({
+    id: '',
+    nome: '',
+    email: '',
+    idade: ''
+  });
+
+  const selecionarAluno = (aluno, opcao) => {
+    setAlunoSelecionado(aluno);
+    (opcao === "Editar") &&
+      abrirFecharModalEditar();
+  };
+
+  const abrirFecharModalIncluir = () => {
+    setModalIncluir(!modalIncluir);
+  };
+
+  const abrirFecharModalEditar = () => {
+    setModalEditar(!modalEditar);
+  };
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado, [name]: value
+    });
+    console.log(alunoSelecionado);
+  };
+
+  const pedidoGet = async () => {
     await axios.get(baseUrl)
       .then(Response => {
         setData(Response.data);
       }).catch(error => {
-        console.log(error)
-      })
-  }
+        console.log(error);
+      });
+  };
+
+  const pedidoPost = async () => {
+    delete alunoSelecionado.id;
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios.post(baseUrl, alunoSelecionado)
+      .then(response => {
+        setData(data.concat(response.data));
+        abrirFecharModalIncluir();
+      }).catch(error => {
+        console.log(error);
+      });
+  };
+
+  const pedidoPut = async () => {
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios.put(baseUrl + "/" + alunoSelecionado.id, alunoSelecionado)
+      .then(response => {
+        var resposta = response.data;
+        var dadosAuxiliar = data;
+        dadosAuxiliar.map(aluno => {
+          if (aluno.id === alunoSelecionado.id) {
+            aluno.nome = resposta.nome;
+            aluno.email = resposta.email;
+            aluno.idade = resposta.idade;
+          }
+        });
+        abrirFecharModalEditar();
+      }).catch(error => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="Aluno-Container">
@@ -26,7 +87,7 @@ function App() {
       <h3>Cadastro de Alunos</h3>
       <header>
         <img src={cadastro} alt='Cadastro' />
-        <button className='btn btn-success'>Incluir novo Aluno</button>
+        <button className='btn btn-success' onClick={() => abrirFecharModalIncluir()}>Incluir novo Aluno</button>
       </header>
       <table className='table table-bordered'>
         <thead>
@@ -46,13 +107,63 @@ function App() {
               <td>{aluno.email}</td>
               <td>{aluno.idade}</td>
               <td>
-                <Button className='btn btn-primary'>Editar</Button> {" "}
-                <Button className='btn btn-danger'>Excluir</Button>
+                <Button className='btn btn-primary' onClick={() => selecionarAluno(aluno, "Editar")}>Editar</Button> {" "}
+                <Button className='btn btn-danger' onClick={() => selecionarAluno(aluno, "Excluir")}>Excluir</Button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <Modal isOpen={modalIncluir}>
+        <ModalHeader>Incluir Alunos</ModalHeader>
+        <ModalBody>
+          <div className='form-group'>
+            <label>Nome: </label>
+            <br />
+            <input type='text' className='form-control' name="nome" onChange={handleChange} />
+            <label>Email: </label>
+            <br />
+            <input type='text' className='form-control' name="email" onChange={handleChange} />
+            <br />
+            <label>Idade: </label>
+            <input type='text' className='form-control' name="idade" onChange={handleChange} />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button className='btn btn-primary' onClick={() => pedidoPost()}>Incluir</Button>{" "}
+          <Button className='btn btn-danger' onClick={() => abrirFecharModalIncluir()}>Cancelar</Button>{" "}
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>Editar Aluno</ModalHeader>
+        <ModalBody>
+          <div className='form-group'>
+            <label>ID: </label>
+            <br />
+            <input type='text' className='form-control' readOnly value={alunoSelecionado && alunoSelecionado.id} />
+            <br />
+            <label>Nome: </label>
+            <br />
+            <input type='text' className='form-control' name="nome" value={alunoSelecionado.nome} onChange={handleChange} />
+            value={alunoSelecionado && alunoSelecionado.nome}
+            <br />
+            <label>Email: </label>
+            <br />
+            <input type='text' className='form-control' name="email" value={alunoSelecionado.email} onChange={handleChange} />
+            value={alunoSelecionado && alunoSelecionado.email}
+            <br />
+            <label>Idade: </label>
+            <input type='text' className='form-control' name="idade" value={alunoSelecionado.idade} onChange={handleChange} />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button className='btn btn-primary' onClick={() => pedidoPut()}>Editar</Button>{" "}
+          <Button className='btn btn-danger' onClick={() => abrirFecharModalEditar()}>Cancelar</Button>{" "}
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
